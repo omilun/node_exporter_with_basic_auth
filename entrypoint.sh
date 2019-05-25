5f27649be80c008a282d5b8ae572dbd435866f81
omid rajabagha <omidrajabagha@gmail.com>
2019-05-25 10:20:47 +0000

    add localhost ip to node exporter default port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo -n "$USERNAME:$PASSWD" > /etc/nginx/htpasswd
 chmod 644 /etc/nginx/htpasswd
 nginx -c /etc/nginx/nginx.conf &
-exec /usr/bin/node_exporter --web.listen-address=":9180" "$@"
+exec /usr/bin/node_exporter --web.listen-address="127.0.0.1:9181" "$@"
