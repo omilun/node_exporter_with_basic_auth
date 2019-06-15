2af932d66b613e28ee28c69dafcc771cd9bdbdb9
omid rajabagha <omidrajabagha@gmail.com>
2019-06-15 13:42:50 +0000

    add openssl command to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo -n "$USERNAME:$PASSWD" > /etc/nginx/htpasswd
+export PASSWD_ENCODED=$(openssl passwd -apr1 $PASSWD)
+echo -n "$USERNAME:$PASSWD_ENCODED" > /etc/nginx/htpasswd
 chmod 644 /etc/nginx/htpasswd
 nginx -c /etc/nginx/nginx.conf &
 exec /usr/bin/node_exporter --web.listen-address="127.0.0.1:9181" "$@"
